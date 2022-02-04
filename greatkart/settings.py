ffd7a9a5efaa2aac22ac521ff8c70f276c63abc6
Kexin Qiu <kristenqiu19@gmail.con>
2022-02-04 01:19:44 -0500

    host

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['greatkart-web-env.eba-6ruwwhnv.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['greatkart-web4-env.eba-3isdhx9t.us-west-2.elasticbeanstalk.com', 'greatkart-web4-env.eba-3isdhx9t.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition

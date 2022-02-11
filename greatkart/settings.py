7423166ed9e2b51b933b1338d9d2e7d590cff7d0
Kexin Qiu <kristenqiu19@gmail.con>
2022-02-11 17:13:51 -0500

    s3

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 #ALLOWED_HOSTS = ['greatkart-web4-env.eba-3isdhx9t.us-west-2.elasticbeanstalk.com']
-ALLOWED_HOSTS = ['numarket-env.eba-rmvv9kiv.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['numarket-env.eba-rmvv9kiv.us-west-2.elasticbeanstalk.com', '*']
 
 # Application definition
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'carts',
     'orders',
     'admin_honeypot',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -67,7 +68,7 @@ ROOT_URLCONF = 'greatkart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,6 +111,16 @@ else:
         }
     }
 
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'ebdb',
+#             'USER': 'numarketadmin',
+#             'PASSWORD': 'Qkx123456789',
+#             'HOST': 'aanz7gsad3gttc.c78mq81ansm3.us-west-2.rds.amazonaws.com',
+#             'PORT': 5432,
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -135,7 +146,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Eastern'
 
 USE_I18N = True
 
@@ -147,17 +158,40 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = 'static'
+#STATIC_ROOT = 'static'
+# STATICFILES_DIRS = [
+#     'greatkart/static',
+# ]
+
+
+# AWS S3 Static Files Configuration
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_LOCATION = 'static'
+
 STATICFILES_DIRS = [
     'greatkart/static',
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'greatkart.media_storages.MediaStorage'
+
 
 # python manage.py collectstatic
 
 #media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 
 from django.contrib.messages import constants as messages

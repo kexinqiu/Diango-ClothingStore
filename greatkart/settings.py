c4357c6c828b6ba7ca93bd1ed16707cabd82458e
Kexin Qiu <kristenqiu19@gmail.con>
2022-02-12 02:34:09 -0500

    images

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -182,6 +182,7 @@ STATICFILES_DIRS = [
     'greatkart/static',
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATIC_ROOT = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'greatkart.media_storages.MediaStorage'
